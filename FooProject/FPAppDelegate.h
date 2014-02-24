da660f3cfd29437697c2e2b6b76380925264ce3c
Rachel Bobbins <rachelheidi@gmail.com>
2014-02-23 20:39:14 -0800

    Initial Commit

---
+++ FooProject/FPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FPAppDelegate.h
+//  FooProject
+//
+//  Created by Rachel Bobbins on 2/23/14.
+//  Copyright (c) 2014 Rachel Bobbins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
